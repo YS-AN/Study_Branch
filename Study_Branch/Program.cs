d56cbb462f8ecb7b9874f0fc5e5b9249ed2e74c3
상해꽃 이 <mxj1234@naver.com>
2023-04-17 20:17:49 +0900

    docs : 만들고싶은게임 초안

--- Study_Branch/Program.cs
+++ Study_Branch/Program.cs
@@ -4,7 +4,13 @@
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello, World!");
+			/*
+			 *  만들고 싶은 게임장르: 디펜스
+			 *  일정구간을 몬스터가 돌면 타워를 이용하여 몬스터를 잡는 형식
+			 *  
+			 * 
+			 * 
+			 */
 		}
 	}
 }
