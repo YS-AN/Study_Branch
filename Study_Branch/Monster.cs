a3e86cf47a5c886de7a8dcf1c794e47e94b80acc
경빈 박 <rudqls1123@naver.com>
2023-04-17 20:29:00 +0900

    feat : 박경빈 메이플 몬스터 추가

---
+++ Study_Branch/Monster.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Study_Branch
+{
+    public class Monster
+    {
+        public string name;
+        public int hp;
+        public int ap;
+
+        public void GetDamage(int damage)
+        {
+            hp -= damage;
+            Console.WriteLine("몬스터가 공격당합니다.");
+        }
+    }
+}
