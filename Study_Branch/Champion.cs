b78faa4f7484609fc80b19155afca904828e89bc
woochangmin <rebuilding33@gmail.com>
2023-04-17 20:17:08 +0900

    feat : 챔피언기본값설정

--- Study_Branch/Champion.cs
+++ Study_Branch/Champion.cs
@@ -8,5 +8,18 @@ namespace Study_Branch
 {
     internal class Champion
     {
+        protected int hp = 100;
+        protected int mp = 30;
+        private int attackpoint;
+        private int ammor;
+
+        public Champion()
+        {
+            this.hp = hp;
+            this.mp = mp;
+            this.attackpoint = attackpoint;
+            this.ammor = ammor;
+
+        }
     }
 }
